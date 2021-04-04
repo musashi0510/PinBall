bcce6227d3cecf19bcb4a6098f5929ed248b2391
musashi0510 <78544643+musashi0510@users.noreply.github.com>
2021-04-05 01:08:53 +0900

    進捗状況報告
    Lesson5のChapter9終了
    課題に作成中

--- Assets/BrightnessRegulator.cs
+++ Assets/BrightnessRegulator.cs
@@ -8,26 +8,26 @@ public class BrightnessRegulator : MonoBehaviour
 	Material myMaterial;
 
 	//Emissionの最小値
-	private float minEmission = 0.3;
+	private float minEmission = 0.3f;
 	//Emissionの強度
-	private float magEmission = 0.2f;
+	private float magEmission = 2.0f;
 	//角度
 	private int degree = 0;
 	//発光速度
 	private int speed = 10;
 	//ターゲットのデフォルトの色
-	Color defaultColor = Color.White;	
+	Color defaultColor = Color.white;	
 
-    // Start is called before the first frame update
+    //Use this for initialization
     void Start()
     {
 	//タグによって光らせる色を変える
 	if(tag == "SmallStarTag")
 	{
-		this.defaultColor = Color.White;
+		this.defaultColor = Color.white;
 	}
 	else if(tag == "LargeStarTag")
-	}
+	{
 		this.defaultColor = Color.yellow;
 	}
 	else if(tag == "SmallCloudTag" || tag == "LargeCloudTag")
@@ -48,17 +48,17 @@ public class BrightnessRegulator : MonoBehaviour
         if(this.degree >= 0)
 	{
 		//光らせる強度を計測する
-		Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin(this.degree * Mathf.Deg2Rad) * this.magEmission);
+		Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin (this.degree * Mathf.Deg2Rad) * this.magEmission);
 
 		//エミッションに色を設定する
 		myMaterial.SetColor("_EmissionColor",emissionColor);
 
 		//現在の角度を小さくする
-		this.degree -= this.Speed;
+		this.degree -= this.speed;
 	}
     }
 	//衝突時に呼ばれる関数
-	void 0nCollisionEnter(Collision other)
+	void OnCollisionEnter(Collision other)
 	{
 		//角度を180に設定
 		this.degree = 180;
